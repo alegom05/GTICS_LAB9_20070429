61f34d0981d8bc5ea506f4d371277b8411ccb17d
alegom05 <146905500+alegom05@users.noreply.github.com>
2024-11-09 13:40:05 -0500

    bd

--- Meal.sql
+++ Meal.sql
@@ -4,26 +4,33 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema meal
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+DROP DATABASE IF EXISTS `meal`;
+CREATE SCHEMA IF NOT EXISTS `meal` DEFAULT CHARACTER SET utf8 ;
+USE `meal` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Comidas`
+-- Table `meal`.`Comidas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Comidas` (
-  `idComidas` INT NOT NULL,
-  `nombre` VARCHAR(45) NULL,
-  `categoria` VARCHAR(45) NULL,
-  `favorito` INT NULL,
-  PRIMARY KEY (`idComidas`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `meal`.`Comidas` (
+  `idMeal` INT NOT NULL AUTO_INCREMENT,
+  `strMeal` VARCHAR(100) NULL,
+  `strDrinkAlternate` VARCHAR(100) NULL,
+  `strCategory` VARCHAR(45) NULL,
+  `strArea` VARCHAR(45) NULL,
+  `strTags` VARCHAR(100) NULL,
+  `strInstructions` TEXT NULL,
+  `strMealThumb` VARCHAR(255) NULL,
+  `strIngredient1` VARCHAR(100) NULL,
+  `strIngredient2` VARCHAR(100) NULL,
+  `strIngredient3` VARCHAR(100) NULL,
+  `strIngredient4` VARCHAR(100) NULL,
+  `favorite` INT NULL,
+  PRIMARY KEY (`idMeal`)
+) ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
